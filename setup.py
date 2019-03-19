dde2b8a4b24660cbc562e5febfef0d3644d41e71
Orlando Acevedo <38707267+oxah@users.noreply.github.com>
2019-03-19 13:02:52 -0700

    Release/1.0.6 (#6)
    * Create Dockerfile
    
    * Feature/dockerfile (#4)
    
    * Fixed docker build.
    
    Removed windows requirements and change base docker to python.
    
    * Update requirements.txt
    
    * Update web_id_helper.py
    
    * Update web_id_exception.py
    
    * Update web_id_string_type.py
    
    * Update web_id_info.py
    
    * Update __init__.py
    
    * Update web_id_type.py
    
    * Update web_id_helper.py
    
    * Python code formatter Black
    
    black -l 80 osisoft
    black -l 80 setup.py
    
    * Black updates to test.
    
    * removed tabs from test
    
    * Update setup.py
    
    * removed tabs
    
    * Revert "removed tabs"
    
    This reverts commit 34d799513cf8dcc4b5a9630e451238a01df2c66d.
    
    * Revert "Update setup.py"
    
    This reverts commit 327742bbf00cc137bcf61b37385d8c56a6918022.
    
    * Revert "removed tabs from test"
    
    This reverts commit 05cab087f2338e11adee3ccacc188ce05dcbb206.
    
    * Revert "Black updates to test."
    
    This reverts commit e348d0599f0257f907e615eca1c7c142707cea38.
    
    * Revert "Python code formatter Black"
    
    This reverts commit a740399ec698dab0aba27b502120a346ed1955e7.
    
    * Revert "Update web_id_helper.py"
    
    This reverts commit 1eaaa4b530e0945db3d89f8a10f4defa0e6cc7ce.
    
    * Revert "Update web_id_type.py"
    
    This reverts commit 1a42c9588c8266120fea325f17011f469a190af3.
    
    * Revert "Update __init__.py"
    
    This reverts commit 9aa788b08bb17b6ada14fd4f0693fce539432cbd.
    
    * Revert "Update web_id_info.py"
    
    This reverts commit b6e3a75b586beb08b833867d5b9400ae83bfe7fc.
    
    * Revert "Update web_id_string_type.py"
    
    This reverts commit bdc09552d09e90ace5cf50a41feab10fb11e94ef.
    
    * Revert "Update web_id_exception.py"
    
    This reverts commit 73f48a7c519db629343ff5d4099f9abd4e6e39d5.
    
    * Revert "Update web_id_helper.py"
    
    This reverts commit 3c5d372237da684e5a460a0e3c0992f1804dc1ee.
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Skip most test.
    
    * Update setup.py
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@
 from setuptools import setup, find_packages
 
 NAME = "osisoft.pidevclub.piwebapi"
-VERSION = "1.1.5"
+VERSION = "1.1.6"
 # To install the library, run the following
 #
 # python setup.py install
